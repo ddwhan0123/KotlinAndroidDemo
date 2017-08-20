a7cb131dd71e6bf8a027160fab5922e2aac2809b
王佳杰 <526357367@qq.com>
2017-08-20 18:01:27 +0800

    添加测试代码

--- app/src/main/java/demo/wjj/com/kotlinandroiddemo/bean/Person.kt
+++ app/src/main/java/demo/wjj/com/kotlinandroiddemo/bean/Person.kt
@@ -27,6 +27,20 @@ class Person {
         val c: Int
         c = 10
         Toast.makeText(context, (a + b + (c)).toString(), Toast.LENGTH_SHORT).show()
+        printValue()
+    }
+
+    fun printValue() {
+        var a: Int = 10
+        var b: Long = 10
+        var c: Short = 10
+        var d: Byte = 10
+        var e: Double = 10.0
+        var f: Float = 10.0f
+        var g: Int = f.toInt()
+        var h: Boolean = true
+        var i: Char = '1'
+        println("a " + a + " b " + b + " c " + c + " d " + d + " e " + e + " f " + f + " g " + g + " h " + h + " i " + i)
     }
 
 }
