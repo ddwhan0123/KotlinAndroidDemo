dffa9efe395b758a8f4aef949302b42d1af96bf2
王佳杰 <526357367@qq.com>
2017-09-11 01:40:28 +0800

    访问作用域

--- app/src/main/java/demo/wjj/com/kotlinandroiddemo/activity/SecondActivity.kt
+++ app/src/main/java/demo/wjj/com/kotlinandroiddemo/activity/SecondActivity.kt
@@ -1,22 +1,39 @@
 package demo.wjj.com.kotlinandroiddemo.activity
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Context
+import android.util.Log
 
 import demo.wjj.com.kotlinandroiddemo.R
+import demo.wjj.com.kotlinandroiddemo.base.BaseActivity
 
-class SecondActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+class SecondActivity : BaseActivity() {
+    override fun getLayoutID(): Int {
+        return R.layout.activity_second
     }
+
+    override fun init() {
+        Log.d("--->","SecondActivity init")
+    }
+
+    override fun setListener() {
+        Log.d("--->","SecondActivity setListener")
+    }
+
 
     override fun onResume() {
         super.onResume()
+        Log.d("--->", "SecondActivity onResume")
+        showToast(this, "我是第二页的 Toast", 1)
     }
 
     override fun onPause() {
         super.onPause()
+        Log.d("--->", "SecondActivity onPause")
     }
+
+    override fun showToast(context: Context, msg: String, time: Int) {
+        Log.d("--->", "SecondActivity showToast 被重写 ")
+        Log.d("--->", " VersionName " + showAppVersionName(this))
+    }
+
 }
--- app/src/main/java/demo/wjj/com/kotlinandroiddemo/base/BaseActivity.kt
+++ app/src/main/java/demo/wjj/com/kotlinandroiddemo/base/BaseActivity.kt
@@ -1,35 +1,84 @@
 package demo.wjj.com.kotlinandroiddemo.base
 
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
 
 /**
  * Created by wangjiajie on 2017/9/11.
  */
-class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutID())
+        init()
+        setListener()
+        Log.d("--->", "BaseActivity onCreate")
     }
+
+    abstract fun getLayoutID(): Int
+    abstract fun init()
+    abstract fun setListener()
 
     override fun onResume() {
         super.onResume()
+        Log.d("--->", "BaseActivity onResume")
     }
 
     override fun onStart() {
         super.onStart()
+        Log.d("--->", "BaseActivity onStart")
     }
 
     override fun onPause() {
         super.onPause()
+        Log.d("--->", "BaseActivity onPause")
     }
 
     override fun onStop() {
         super.onStop()
+        testPrivate()
+        Log.d("--->", "BaseActivity onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("--->", "BaseActivity onRestart")
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.d("--->", "BaseActivity onDestroy")
+    }
+
+    open fun showToast(context: Context, msg: String, time: Int) {
+        Toast.makeText(context, msg, time).show()
+        Log.d("--->", "BaseActivity showToast")
+    }
+
+    //获取appVersionName
+    protected fun showAppVersionName(cotext: Context): String {
+        var versionName = ""
+        try {
+            val pm: PackageManager = cotext.packageManager
+            val pi: PackageInfo = pm.getPackageInfo(cotext.packageName, 0)
+            versionName = pi.versionName
+            if (versionName == null) {
+                return ""
+            }
+        } catch (e: Exception) {
+            Log.e("VersionInfo", "Exception", e)
+        }
+        return versionName
+    }
+
+    private fun testPrivate() {
+        Log.d("--->", "testPrivate")
     }
 }
